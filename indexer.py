802b2d993cd70f4db98d1fe23376a00ef1997d1e
Mohammad Mirzaei <mirzaem1@uci.edu>
2024-05-06 19:56:09 -0700

    URLs of files will be assigned a unique ID

--- indexer.py
+++ indexer.py
@@ -9,7 +9,23 @@ inverted_index = {}
 URLids = {}
 
 def main():
-	pass
+    # List of all the files in the directory
+    files = os.listdir("sample_files") 
+    counter = 0
+
+    for file in files : 
+        # If file is not JSON do not read it
+        if not file.endswith(".json"):
+            continue
+        with open(f"small_dataset/{file}",) as the_file:
+            # Load the json file into data
+            data = json.load(the_file) 
+
+            # Save the URL id
+            url = data['url']
+            URLids[url] = counter;
+            counter += 1
+
 
 if __name__ == "__main__":
     main()
