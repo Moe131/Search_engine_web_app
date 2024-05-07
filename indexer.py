aeaa2f031cc8009b1c55c33973d926c4b773e185
Mohammad Mirzaei <mirzaem1@uci.edu>
2024-05-06 19:53:17 -0700

    tokneizer.py was added

---
+++ indexer.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup 
+import json
+import os
+from tokenizer import *
+
+# Dictionary of tokens and their postings
+inverted_index = {}
+# Dictionary of URL and their IDs
+URLids = {}
+
+def main():
+	pass
+
+if __name__ == "__main__":
+    main()
+
---
+++ tokenizer.py
@@ -0,0 +1,30 @@
+# Time complexity of this method is constant O(1) because it only
+# iterates through a constant sized string
+def isAlphaNum(ch:str) -> bool:
+    ''' Checks if a character is number or american letter  '''
+    english_letters= set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+    if ch in english_letters:
+        return True
+    else:
+        return False
+
+
+# Time complexity of this method is linear  O(n). The method iterates through 
+# the characters(bytes) one by one.
+def tokenize(text: str) -> dict:
+	''' reads in a text and returns a list of the tokens in that string '''
+	tokens = dict()
+	token = ""
+	for ch in text + " ":
+		if isAlphaNum(ch):
+			token += ch
+		else:
+			if token != "":
+				if  len(token) > 1 :
+					word = token.lower()
+					if word in tokens.keys():
+						tokens[word] += 1
+					else:
+						tokens[word] = 1
+				token = "" 
+	return tokens
