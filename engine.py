8335cf38a69a89f68bffd87ad5f1564c6f4ece8b
Mohammad Mirzaei <mirzaei_1378@yahoo.com>
2024-05-10 00:54:46 -0700

    Shows search result for  queries of length 1 for now based on inverted index

--- engine.py
+++ engine.py
@@ -1,18 +1,32 @@
 from indexer import load_inverted_index
+from porter2stemmer import Porter2Stemmer
+
 
 # Dictionary of tokens and their postings
 inverted_index = {}
 # Dictionary of URL and their IDs
-URLids = {}
+documents = {}
 
 def process(query):
     """ Processes the query by using the inverted index and
       returns a list of URLs in order of relevance to the query"""
-    return list()
+    # Now just handles a single word query
+    result = list()
+    stemmer = Porter2Stemmer()
+    for word in query.split():
+        stem = stemmer.stem(word)
+        if stem in inverted_index:
+            for docID,freq in inverted_index[stem]:
+                result.append(documents[docID])
+    return result
 
 
 def display(urls):
     """ Gets a list of result URLs as parameter and displays them in order """
+    print("-" * 50)
+    if len(urls) == 0:
+        print("No results found for your query")
+        return
     counter = 1
     for url in urls:
         print(f"{counter}. {url}\n")
@@ -21,8 +35,8 @@ def display(urls):
 
 def run():
     """ The search engine starts running and asks the user to enter queries"""
-    global inverted_index , URLids
-    inverted_index , URLids = load_inverted_index()
+    global inverted_index , documents
+    inverted_index , documents = load_inverted_index()
     while (True):
         query = input("Enter your search query:     ")
         if query.isspace() or query == "":
--- indexer.py
+++ indexer.py
@@ -7,7 +7,7 @@ from porter2stemmer import Porter2Stemmer
 # Dictionary of tokens and their postings
 inverted_index = {}
 # Dictionary of URL and their IDs
-URLids = {}
+documents = {}
 # Counts from zero for URL ids
 counter = 0
 
@@ -15,7 +15,7 @@ counter = 0
 def index(file):
     """ Indexes a JSON file with a given path and stores the inverted index
       globaly as a mapping of a token to a list of tuples of URL ids and frequency"""
-    global inverted_index, URLids, counter
+    global inverted_index, documents, counter
     # If file is not JSON do not read it
     if not file.endswith(".json"):
         return
@@ -25,7 +25,7 @@ def index(file):
 
         # Save the URL id
         url = data['url']
-        URLids[counter] = url
+        documents[counter] = url
         url_id = counter
         counter += 1
 
@@ -47,7 +47,7 @@ def create_report(filepath):
     """ Creates a report of the inverted index in a text file"""
     with open(filepath, 'w') as f:
         f.write(f"Number of unique tokens : {len(inverted_index)} \n"+ "-" * 50 + "\n")
-        f.write(f"Number of documents  : {len(URLids)} \n"+ "-" * 50 + "\n")
+        f.write(f"Number of documents  : {len(documents)} \n"+ "-" * 50 + "\n")
         # Gets file size of the inverted index
         data_file_size = os.path.getsize("data.pickle")
         f.write(f"Inverted Index file size  : {data_file_size/1024} KB\n"+ "-" * 50 + "\n")
@@ -56,19 +56,19 @@ def create_report(filepath):
 def save_inverted_index():
     """ Saves the inverted index dictionaries in a pickle file """
     with open("data.pickle", "wb") as f:
-        pickle.dump((inverted_index, URLids), f)
+        pickle.dump((inverted_index, documents), f)
 
 
 def load_inverted_index(): 
      """ Loads the inverted index dictionaries from the pickle file and returns them"""
      try:
         with open("data.pickle" , "rb") as f:
-            inverted_index, URLids = pickle.load(f)
+            inverted_index, documents = pickle.load(f)
      except FileNotFoundError:
         # If the file doesn't exist
         inverted_index = {}
-        URLids = {}
-     return inverted_index, URLids
+        documents = {}
+     return inverted_index, documents
 
 
 
