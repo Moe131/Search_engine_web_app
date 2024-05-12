055044ab3be7caf72bb0a5efb272ae5cbec1a67d
Mohammad Mirzaei <mirzaei_1378@yahoo.com>
2024-05-12 14:39:10 -0700

    Shows error message when no search results found

--- app.py
+++ app.py
@@ -7,18 +7,20 @@ engine = Engine()
 # Search query
 query = ""
 search_results = []
+has_searched = False
 
 @app.route("/", methods=["POST", "GET"])
 def index():
-    global query, search_results
+    global query, search_results, has_searched
     if request.method == "POST":
         query = request.form['query']
         postings = engine.process(query)
         search_results = engine.get_top_five(postings)
         print(search_results)
+        has_searched = True
         return redirect('/')
     else:
-        return render_template('index.html', urls = search_results)
+        return render_template('index.html', urls = search_results, has_searched=has_searched )
 
 if __name__ == "__main__":
     app.run()
